45c8cbf1a1369c5dce131d2cb93333dcb2cf47be
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-16 15:12:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
+const React = require("react")
+const ReactDOM = require("react-dom")
 
 const root = document.getElementById("root")
 ReactDOM.render(<h1>Hello</h1>, root)
