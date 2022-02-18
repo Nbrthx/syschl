1d8e80927d94b63ce3a12b234596f04435a325ba
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-18 15:08:39 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+function Name(props){
+  return <p>Hello {props.name}</p>
+}
+
+class App extends React.Component{
+  state {
+    name: "Jhon Doe"
+  }
+
+  function change(){
+    setState({ name: state.name })
+  }
+
+  render() {
+    return (
+      <h1>Test App</h1>
+      <Name name={state.name} /><br />
+      <br />
+      <input type="text" value={state.name} />
+      <button onClick={change}>Submit</button>
+    )
+  }
+}
