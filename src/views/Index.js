1aeebe007812679671d13d4d10dcdfa6f16de727
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-19 21:12:29 +0700

    Create Index.js

---
+++ src/views/Index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Cookies from "universal-cookie"
+
+const cookies = new Cookies()
+
+const Index = () => {
+  const user = cookies.get("user")
+
+  if(user != null){
+    return (
+      <div>
+        <h1>Hello {user}</h1>
+        Welcome to React Test App
+      </div>
+    )
+  }
+  return(
+    <div>
+      <h1>Syscuhl App</h1>
+      Not logged in, login on <a href="login">here</a>
+    </div>
+  )
+}
