b68d04150975f854ff8e0441a59d1355e3166e12
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-19 22:34:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React from "react"
 import Cookies from "universal-cookie"
 
+import Index from "./views/Index"
+
 const cookies = new Cookies()
 
-function Name(props) {
-  return (<p>Hello {props.name}</p>)
+function Router(props) {
+  if(window.location.pathname == props.path)
+    return (<{props.page} />)
+  return (<></>)
 }
 
 class App extends React.Component{
-  state = {
-    name: cookies.get("user") || "Jhon Doe"
-  }
-  
-  name = ""
-  change = (e) => {
-    this.name = e.target.value
-  }
-
-  userChange = () => {
-    cookies.set("user", this.name, { path: "/" })
-    this.setState({ name: this.name })
-  }
-
   render() {
     return (
       <div>
-        <h1>Test App</h1>
-        <Name name={this.state.name} /> 
-        <br /><br />
-        <input type="text" value={this.value} onChange={this.change} /><br />
-        <button onClick={this.userChange}>Submit</button>
+        <Router path="/" page="Index" />
       </div>
     )
   }
