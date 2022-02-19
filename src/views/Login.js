acd422ca18ea46cb470e91e5945695b8b2172515
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-19 23:58:59 +0700

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Cookies from "universal-cookie"
 import Pg from "pg"
+import Cjs from "crypto-js"
 
 const cookies = new Cookies()
 const pool = new Pg.Pool({
@@ -13,12 +14,26 @@ const Login = () => {
 
   var name = ""
   var password = ""
+  var msg = ""
+
+  const signIn = () => {
+    pool.query("select uname, pword from users where uname='"+name+"'",
+      (err, data) => {
+        var uname = data.rows[0].uname
+        var pword = cjs.AES.decrypt(data.rows[0].pword).toString(cjs.enc.Utf8)
+        if(uname === name.toLowerCase() && pword === password){
+          cookies.set("user", name.toLowerCase(), { path: "/" })
+          window.location.href = "/"
+        }else msg="Incorrect Input!"
+      }
+  }
 
   if(user != null) window.location.href = "/"
 
   return(
     <div>
       <h1>Syscuhl App</h1>
+       {msg}
        <label>Name</label><br>
        <input type="text" value={name}><br>
        <label>Password</label><br>
