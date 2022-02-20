73b03b8b73c8c10da280abb9cd9971f161e5bd9e
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-20 13:18:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.get("/api", (req, res) => {
   res.json({"name": "Niberthix"})
 })
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+})
+
 app.listen(port, () => console.log("Listen on "+port))
 
