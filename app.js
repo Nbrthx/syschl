546b22aa1d3c3dda45a1e85e404469e77b464dbf
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-20 09:14:24 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+  "name": "nextix",
+  "description": "School Application",
+  "version": "0.0.0",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "build": "cd client && npm install && npm start"
+  },
+  "author": "Ektada Benabi",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^5.0.0",
+    "pg": "^8.7.3"
+  }
+}
