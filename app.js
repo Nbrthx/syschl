98b87cc7e9f10963b214b5c8fced3859fc3726df
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-20 14:17:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express")
 const path = require("path")
+const pg = require("pg")
 const app = express()
 
 const port = process.env.PORT || 3001
 
+const config = {
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false }
+}
+const pool = pg.Pool(config)
+
 app.use(express.static(__dirname+"/client/build"))
 
 app.get("/api", (req, res) => {
-  res.json({"name": "Niberthix"})
+  if(req.query.for == "login"){
+    const user = req.query.user
+    pool.query("select uname, pword from users where uname='"+user+"'", (err, data) => {
+      res.json(data.rows[0])
+    })
+  }
 })
 
 app.get("*", (req, res) => {
