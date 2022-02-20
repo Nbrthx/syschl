63a2541b9a9b1d4150089017b0be5d26358f6fcf
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-20 13:32:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/api", (req, res) => {
 })
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'))
+  res.sendFile(path.join(__dirname+'/client/build', 'index.html'))
 })
 
 app.listen(port, () => console.log("Listen on "+port))
