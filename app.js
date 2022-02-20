c6e64a16926222a027fc45303ebfbc04e6414aa9
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-20 13:53:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express()
 
 const port = process.env.PORT || 3001
 
+app.use(express.static(__dirname+"/client/build"))
+
 app.get("/api", (req, res) => {
   res.json({"name": "Niberthix"})
 })
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build', 'index.html'))
+  res.sendFile(path.join(__dirname+"/client/build", "index.html"))
 })
 
 app.listen(port, () => console.log("Listen on "+port))
