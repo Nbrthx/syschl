385381c8f9cb478fa1f077d704cfd58fb0e7ac84
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-20 14:47:32 +0700

    Update Login.js

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -8,22 +8,30 @@ const Login = () => {
   const user = cookies.get("user")
 
   const [name, setName] = React.useState()
+  const [password, setPassword] = React.useState()
+
   const nameChange = (e) => {
     setName(e.target.value)
   }
-  const [password, setPassword] = React.useState()
   const passwordChange = (e) => {
     setPassword(e.target.value)
   }
   const [msg, setMsg] = React.useState()
 
   const signIn = () => {
-      var uname = "nbrthx"
-      var pword = "12345678" || Cjs.AES.decrypt("12345678", "justlnh").toString(Cjs.enc.Utf8)
-      if(uname === name.toLowerCase() && pword === password){
-        cookies.set("user", name.toLowerCase(), { path: "/" })
-        window.location.href = "/"
-      }else setMsg("Incorrect Input!")
+    var result = ""
+    React.useEffect(() => {
+      fetch("/sapi?for=login&&user="+name)
+        .then((res) => res.json())
+        .then((data) => result = data)
+    }, [])
+
+    var uname = result.uname
+    var pword = Cjs.AES.decrypt(result.pword, process.env.PSPH).toString(Cjs.enc.Utf8)
+    if(uname === name.toLowerCase() && pword === password){
+      cookies.set("user", name.toLowerCase(), { path: "/" })
+      window.location.href = "/"
+    }else setMsg("Username or Password Incorrect!")
   }
 
   if(user != null) window.location.href = "/"
