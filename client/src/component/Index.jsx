f43676b4f136e45c691d4514d1985a7b1001125e
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-26 20:50:07 +0700

    Update Index.jsx

--- client/src/component/Index.jsx
+++ client/src/component/Index.jsx
@@ -5,6 +5,10 @@ const cookies = new Cookies()
 
 const Index = () => {
   const user = cookies.get("user")
+  if(user == null){
+    window.location.href = "/login"
+    return null
+  }
 
   const [userData, setUserData] = React.useState({
     name: "null",
@@ -29,34 +33,20 @@ const Index = () => {
   window.onload = () => getData()
 
 
-  if(user != null){
-    return (
-      <div className="dashboard">
-        <div className="card">
-          <h1>Hello {userData.name}</h1>
-          Welcome to React Test App<br />
-          <div class="data-diri">
-            Nama: {userData.name}<br />
-            Absen: {userData.absen}<br />
-            Skor: {userData.skor}<br />
-            Kelas: {userData.kelas}
-          </div>
+  return (
+    <div className="dashboard">
+      <div className="card">
+        <h1>Hello {userData.name}</h1>
+        Welcome to React Test App<br />
+        <div class="data-diri">
+          Nama: {userData.name}<br />
+          Absen: {userData.absen}<br />
+          Skor: {userData.skor}<br />
+          Kelas: {userData.kelas}
         </div>
       </div>
-    )
-  }
-  else
-    return(
-      <div className="dashboard">
-        <div className="card">
-          <h1>Syscuhl App</h1>
-          <b>Wellcome to E-Syschool</b><br />
-          You have to login before use our service.<br />
-          Login on <a href="login">here</a>
-          <br />{process.env}
-        </div>
-      </div>
-    )
+    </div>
+  )
 }
 
 export default Index
