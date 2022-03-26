f3bf2d7f97511f775b9e5c3ca792cea5c77d0a59
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-03-27 01:35:21 +0700

    Update cktool.js

--- src/component/cktool.js
+++ src/component/cktool.js
@@ -18,7 +18,7 @@ export const cktool = {
         }
 
         const resj = await fetch("/decrypt", option)
-        const decc = (await resj.json()).result
+        const decc = (await resj.json()).result.toString()
         console.log(decc)
         return decc
     },
