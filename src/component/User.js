e087f32161b00a7d2b3a32810e4641d4f1461677
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-03-31 16:57:59 +0700

    Update User.js

--- src/component/User.js
+++ src/component/User.js
@@ -1,7 +1,11 @@
 const cstring = document.cookie
 const cookies = cstring ? cstring
-    .split("; ")
-    .map(v => v.split("user=")) : ""
+  .split(';')
+  .map(v => v.split('='))
+  .reduce((acc, v) => {
+    acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+    return acc;
+  }, {}) : ""
 
 function User(){ return cookies }
 
